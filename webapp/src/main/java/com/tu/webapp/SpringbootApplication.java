82571241a9ab7efde20d2110ed9773f70f78dfd5
HQ01UG982 <hq01ug982@mb.com>
2019-04-19 14:15:25 +0800

    Initial commit

---
+++ webapp/src/main/java/com/tu/webapp/SpringbootApplication.java
@@ -0,0 +1,30 @@
+package com.tu.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @Description
+ * @Classname SpringBootApplication
+ * @Date 2019/4/19 11:28
+ * @Created by tuyongjian
+ *
+ * 还没有配置数据库连接时候，启动报以下错误，
+ * Failed to configure a DataSource: 'url'
+ * attribute is not specified and no embedded datasource could be configured.
+ *
+ * 添加exclude={DataSourceAutoConfiguration.class} 即可启动成功
+ *
+ */
+
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootApplication.class,args);
+
+    }
+}
