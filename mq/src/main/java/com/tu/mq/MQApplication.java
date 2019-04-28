2a57f2075701e3e07493ace58cd254d6b4e4c804
HQ01UG982 <hq01ug982@mb.com>
2019-04-28 16:21:14 +0800

    添加 kafka 代码

---
+++ mq/src/main/java/com/tu/mq/MQApplication.java
@@ -0,0 +1,21 @@
+package com.tu.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @Description 消息启动类
+ * @Classname MQApplication
+ * @Date 2019/4/28 15:28
+ * @Created by tuyongjian
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class MQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MQApplication.class,args);
+    }
+
+
+}
---
+++ mq/src/main/java/com/tu/mq/kafka/KafkaConsumer.java
@@ -0,0 +1,10 @@
+package com.tu.mq.kafka;
+
+/**
+ * @Description
+ * @Classname KafkaConsumer kafka消费者
+ * @Date 2019/4/28 16:06
+ * @Created by tuyongjian
+ */
+public class KafkaConsumer {
+}
