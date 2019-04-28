56761bdddd91e44eecd7b7bfdab38b08be8e90b3
HQ01UG982 <hq01ug982@mb.com>
2019-04-28 15:13:49 +0800

    添加redis代码

---
+++ redis/src/main/java/com/tu/redis/RedisApplication.java
@@ -0,0 +1,20 @@
+package com.tu.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @Description
+ * @Classname RedisApplication redis 服务的启动类
+ * @Date 2019/4/28 14:05
+ * @Created by tuyongjian
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class RedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisApplication.class,args);
+    }
+
+}
---
+++ redis/src/main/java/com/tu/redis/controller/RedisTestAction.java
@@ -0,0 +1,31 @@
+package com.tu.redis.controller;
+
+import com.tu.redis.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Description
+ * @Classname RedisTestAction
+ * @Date 2019/4/28 14:28
+ * @Created by tuyongjian
+ */
+@RestController
+public class RedisTestAction {
+
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @ResponseBody
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    public String test(){
+        redisUtil.set("test","adfafa");
+        return redisUtil.get("test").toString();
+    }
+
+    @GetMapping("index")
+    public String index(){
+        return "Hello world";
+    }
+
+}
