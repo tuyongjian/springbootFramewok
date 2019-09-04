355cf746662bb02cad89dee41f890a083ebd04b8
HQ01UG982 <hq01ug982@mb.com>
2019-09-04 13:49:07 +0800

    改jar包

--- redis/src/main/java/com/tu/redis/RedisApplication.java
+++ redis/src/main/java/com/tu/redis/RedisApplication.java
@@ -1,8 +1,11 @@
 package com.tu.redis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * @Description
@@ -10,7 +13,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @Date 2019/4/28 14:05
  * @Created by tuyongjian
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class RedisApplication {
 
     public static void main(String[] args) {
