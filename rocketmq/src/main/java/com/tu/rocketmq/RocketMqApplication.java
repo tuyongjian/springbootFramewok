9414e212a56699e96e66b2283e359d72d261641d
HQ01UG982 <hq01ug982@mb.com>
2020-04-16 16:07:37 +0800

    新增mycat rocketmq模块

--- rocketmq/src/main/java/com/tu/rocketmq/RocketMqApplication.java
+++ rocketmq/src/main/java/com/tu/rocketmq/RocketMqApplication.java
@@ -1,9 +1,18 @@
 package com.tu.rocketmq;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 /**
  * @Auther: tuyongjian
  * @Date: 2020/4/16 15:25
  * @Description:
  */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class RocketMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketMqApplication.class,args);
+    }
 }
