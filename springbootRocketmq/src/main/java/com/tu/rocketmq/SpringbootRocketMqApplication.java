7b265d6082898ee5951eb362742f42a49432d417
HQ01UG982 <hq01ug982@mb.com>
2020-04-21 11:19:00 +0800

    rocketmq

---
+++ springbootRocketmq/src/main/java/com/tu/rocketmq/SpringbootRocketMqApplication.java
@@ -0,0 +1,13 @@
+package com.tu.rocketmq;
+
+/**
+ * Hello world!
+ *
+ */
+public class SpringbootRocketMqApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
