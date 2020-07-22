437fa91d1705783c9a0c7c96a521add3554f1cbe
HQ01UG982 <hq01ug982@mb.com>
2020-07-22 17:17:12 +0800

    timetask init

---
+++ timeTask/src/main/java/TimeTask.java
@@ -0,0 +1,10 @@
+/**
+ * @Auther: tuyongjian
+ * @Date: 2020/7/22 17:13
+ * @Description:
+ */
+public class TimeTask {
+    public static void main(String[] args) {
+        System.out.println(111111111);
+    }
+}
